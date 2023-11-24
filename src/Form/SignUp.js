25dfde48d1a711b20b3b0bc408604896a0411976
Shoaib <ShoaibSiddiqui0708@gmail.com>
2023-11-24 21:22:33 +0500

    Fifth task done

--- src/Form/SignUp.js
+++ src/Form/SignUp.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name : '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   const handleSignUpClick = () => {
     console.log('Sign Up button clicked');
+    console.log('Name:', formData.name);
+    console.log('Email:', formData.email);
+    console.log('Password:', formData.password);
   };
 
   return (
@@ -10,12 +27,20 @@ const SignUp = () => {
       <form>
         <h1>Sign Up Form</h1>
         <label>
+          Name:
+          <input
+            type="text" name='name' placeholder='Enter Your Name' className='input-field' onChange={handleInputChange}
+          />
+        </label>
+        <label>
           Email:
-          <input type="email" name='email' placeholder='Enter Your Email' className='input-field' />
+          <input type="email" name='email' placeholder='Enter Your Email' className='input-field' onChange={handleInputChange}
+          />
         </label>
         <label>
           Password:
-          <input type="password" name='password' placeholder='Enter Your password' className='input-field' />
+          <input type="password" name='password' placeholder='Enter Your password' className='input-field' onChange={handleInputChange}
+          />
         </label>
         <button type='button' onClick={handleSignUpClick}>
           Sign Up
