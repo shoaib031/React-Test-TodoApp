263cbee3997308f927cedaf67050e287d8c42605
Shoaib <ShoaibSiddiqui0708@gmail.com>
2023-11-24 19:42:50 +0500

    Second project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import React from 'react';
-import SignUp from './Router/SignUp'
-import SignIn from './Router/SignIn'
-import AddTodo from './Router/AddTodo'
-import UpdateTodo from './Router/UpdateTodo'
-import DeleteTodo from './Router/DeleteTodo'
-import SignOut from './Router/SignOut'
+import SignUpComponent from './Router/SignUp';
+import SignInComponent from './Router/SignIn';
+import AddTodo from './Router/AddTodo';
+import UpdateTodo from './Router/UpdateTodo';
+import DeleteTodo from './Router/DeleteTodo';
+import SignOutComponent from './Router/SignOut';
 
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
+  const handleSignUpClick = () => {
+    console.log('Clicked on Sign Up');
+  };
+
+  const handleSignInClick = () => {
+    console.log('Clicked on Sign In');
+  };
+
+  const handleSignOutClick = () => {
+    console.log('Clicked on Sign Out');
+  };
+
   return (
     <Router>
       <nav>
         <ul>
           <li>
-            <Link to="/signup">Sign Up</Link>
+            <Link to="/signup" onClick={handleSignUpClick}>Sign Up</Link>
           </li>
           <li>
-            <Link to="/signin">Sign In</Link>
+            <Link to="/signin" onClick={handleSignInClick}>Sign In</Link>
+          </li>
+          <li>
+            <Link to="/signout" onClick={handleSignOutClick}>Sign Out</Link>
           </li>
           <li>
             <Link to="/addtodo">Add Todo</Link>
@@ -28,19 +43,16 @@ function App() {
           <li>
             <Link to="/deletetodo">Delete Todo</Link>
           </li>
-          <li>
-            <Link to="/signout">Sign Out</Link>
-          </li>
         </ul>
       </nav>
 
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="addtodo" element={<AddTodo />} />
-        <Route path="updatetodo" element={<UpdateTodo />} />
-        <Route path="deletetodo" element={<DeleteTodo />} />
-        <Route path="signout" element={<SignOut />} />
+        <Route path="/signup" element={<SignUpComponent />} />
+        <Route path="/signin" element={<SignInComponent />} />
+        <Route path="/signout" element={<SignOutComponent />} />
+        <Route path="/addtodo" element={<AddTodo />} />
+        <Route path="/updatetodo" element={<UpdateTodo />} />
+        <Route path="/deletetodo" element={<DeleteTodo />} />
       </Routes>
     </Router>
   );
